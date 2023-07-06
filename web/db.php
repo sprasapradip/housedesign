19239a5b3635454407dbf8b76a186bb39f98a511
sprasapradip <subedipradip199@gmail.com>
2023-07-06 16:53:01 +0545

    adding conn

--- web/db.php
+++ web/db.php
@@ -3,3 +3,11 @@ $servername = "localhost";
 $username = "pradip";
 $password = "**********";
 $dbname = "mydb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
